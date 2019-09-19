fc8d345054ae63766385e8a26de3621b8962e557
Peter Veres <tllvllp@gmail.com>
2019-09-20 00:16:09 +0200

    Removed unnecessary exception
    Signed-off-by: Peter Veres <tllvllp@gmail.com>

--- src/main/java/com/tlvlp/iot/server/unit/service/rpc/MessageFrowardingException.java
+++
@@ -1,7 +0,0 @@
-package com.tlvlp.iot.server.unit.service.rpc;
-
-public class MessageFrowardingException extends Exception {
-    public MessageFrowardingException(String errorMessage) {
-        super(errorMessage);
-    }
-}
